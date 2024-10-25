870e17c1d4d6643edb94e3f35b5409efcc7935c6
Adriana Galindo <adrianagalindosoftware@gmail.com>
2024-10-25 14:17:17 -0400

    Add project files.

---
+++ ServerManagement/Models/ServersRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ServerManagement.Models
+{
+    public class ServersRepository
+    {
+
+    }
+}
---
+++ ServerManagement/Models/ServicesRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ServerManagement.Models
+{
+    public class ServicesRepository
+    {
+        private static List<string> services = new List<string>()
+        {
+            "Design 1",
+            "Development 2",
+            "implement 3"
+
+        };
+        public static List<string> GetServices() => services;
+    }
+}
